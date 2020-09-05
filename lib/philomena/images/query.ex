9943c217e63c72a47c5badcd9cfcec9aead5055d
Dr. Romulus <68035188+drromulus@users.noreply.github.com>
2020-09-04 22:30:44 -0500

    Update query.ex
    Replace wilson score with romulus score

--- lib/philomena/images/query.ex
+++ lib/philomena/images/query.ex
@@ -70,7 +70,7 @@ defmodule Philomena.Images.Query do
     [
       int_fields:
         ~W(id width height comment_count score upvotes downvotes faves uploader_id faved_by_id tag_count pixels size),
-      float_fields: ~W(aspect_ratio wilson_score),
+      float_fields: ~W(aspect_ratio romulus_score),
       date_fields: ~W(created_at updated_at first_seen_at),
       literal_fields:
         ~W(faved_by orig_sha512_hash sha512_hash uploader source_url original_format mime_type),
